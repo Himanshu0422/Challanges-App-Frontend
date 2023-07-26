edd3dadd8fe4480656708b2e34a28662e82ec8ad
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-26 22:07:20 +0530

    register

---
+++ src/pages/Register.js
@@ -0,0 +1,10 @@
+import RegisterForm from "../components/RegisterForm";
+
+
+export default function Register() {
+    return (
+        <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+            <RegisterForm />
+        </div>
+    )
+}
