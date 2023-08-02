ea1bc4deb0427ed31954d815874af116467a8c94
Himanshu0422 <himanshumittal035@gmail.com>
2023-08-02 23:55:46 +0530

    added redux in layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
+import { setDifficulty, setSearch } from '../redux/questionsSlice';
 
 const Layout = ({ children }) => {
 
     const location = useLocation();
-
+    const dispatch = useDispatch();
     const [sidebar, setSidebar] = useState(false);
     const handleSidebar = () => {
         setSidebar(!sidebar);
     };
+
+    const changeHandler = (e) => {
+        dispatch(setSearch(e.target.value));
+    }
+    
+    const changeDiffHandler = (e) => {
+        dispatch(setDifficulty(e.target.value));
+    }
 
     const questionMenu = [
         {
@@ -55,7 +65,7 @@ const Layout = ({ children }) => {
                 </div>
             </div>
             <div className='max-md:flex flex-col hidden'>
-                <div onClick={handleSidebar} className='block lg:hidden cursor-pointer mt-2 ml-2 z-10'>
+                <div onClick={handleSidebar} className='block lg:hidden cursor-pointer mt-2 ml-2 z-10 w-7'>
                     {sidebar ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
                 </div>
                 <ul className={sidebar ? 'fixed left-0 bg-opacity-50 backdrop-blur-lg w-[60%] h-full ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
@@ -71,31 +81,33 @@ const Layout = ({ children }) => {
                 </ul>
             </div>
             <div className="border border-white mx-1 max-md:hidden"></div>
-            <div className='md:w-[85%] max-md:mt-3 max-md:mx-2'>
-                <div className='flex w-full md:h-[15%]'>
-                    <div className='w-[80%] h-full inline-flex items-center'>
+            <div className='md:w-[85%] max-md:mt-3 max-md:mx-2 max-md:h-full'>
+                <div className='flex w-full md:h-[18%]'>
+                    <div className='w-[80%] md:h-[90%] max-md:h-full inline-flex items-center'>
                         <AiOutlineSearch className='h-6 w-6' />
                         <input
                             className='w-full h-full bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                             type='text'
                             placeholder='Seacrh'
+                            onChange={changeHandler}
                         />
                     </div>
-                    <div className="border border-white"></div>
-                    <div className='w-[20%] h-full'>
+                    <div className="border border-white h-[90%]"></div>
+                    <div className='w-[20%] md:h-[90%] max-md:h-full'>
                         <select
                             className="w-full h-full bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             id="difficulty"
                             name="difficulty"
+                            onChange={changeDiffHandler}
                         >
-                            <option disabled value=''>Difficulty</option>
+                            <option disabled selected>Difficulty</option>
                             <option value="Easy">Easy</option>
                             <option value="Medium">Medium</option>
                             <option value="Difficult">Difficult</option>
                         </select>
                     </div>
                 </div>
-                <div className='h-[85%] md:mt-4 md:ml-2 max-md:mt-2 max-md:ml-2'>
+                <div className='mt-5 mb-5 md:h-[75%] max-md:mt-4 max-md:ml-2 max-md:h-[calc(100vh-18rem)]'>
                     {children}
                 </div>
             </div>
