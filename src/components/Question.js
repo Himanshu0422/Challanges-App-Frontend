edf56aefd3b981010513ce65737edd9c0532e603
Himanshu0422 <himanshumittal035@gmail.com>
2023-08-03 10:07:16 +0530

    question

---
+++ src/components/Question.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Question = ({ ques, i }) => {
+
+    const navigate = useNavigate();
+    const { _id, name, difficulty } = ques;
+    return (
+        <div
+            className='w-[30%] max-lg:w-[45%] max-md:w-[45%] max-sm:w-[90%] h-[37%] cursor-pointer rounded-xl button-background mx-2 my-8 hover:transform hover:scale-y-110 hover:origin-top hover:transition-transform'
+            onClick={() => navigate(`/challenge/${_id}`)}
+        >
+            <div className='flex flex-col justify-evenly h-full'>
+                <div className='flex justify-between px-5'>
+                    <img
+                        src='https://reactchallenges.live/20adfb35ae6a6e5817bf.png'
+                        alt='react'
+                        height={50}
+                        width={50}
+                    />
+                    <div className='text-2xl pr-4 text-gray-200'>{i + 1}. {name}</div>
+                </div>
+                <div className='flex justify-between px-10 text-md'>
+                    <div className='border-width rounded-lg p-1'>
+                        <span className='text-green-200 md:mr-3'>©</span>
+                        UseState
+                    </div>
+                    <div className='flex text-sm p-1'>
+                        <div>Difficulty:</div>
+                        <div className='text-gray-300'>{difficulty}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Question
