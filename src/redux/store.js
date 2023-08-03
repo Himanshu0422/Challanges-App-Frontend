0012fb1f9ea775397d3ae52df8abdc2fa1b040a9
Himanshu0422 <himanshumittal035@gmail.com>
2023-08-03 15:17:56 +0530

    alert slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
-
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { userSlice } from "./userSlice";
 import { questionsSlice } from './questionsSlice';
+import { alertsSlice } from "./alertsSlice";
 
 const rootReducer = combineReducers({
     user: userSlice.reducer,
-    question: questionsSlice.reducer
+    question: questionsSlice.reducer,
+    alerts: alertsSlice.reducer
 });
 
 const store = configureStore({
