e6f58939c028745265adb0f2e00987446528dc72
himanshu0422 <himanshumittal035@gmail.com>
2024-12-13 22:16:53 +0530

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ export default function Home() {
     }, []);
 
     return (
-        <main className="text-white flex justify-center items-center max-lg:flex-col mx-10 min-h-[calc(100vh-6rem)]">
+        <main className="text-black flex justify-center items-center max-lg:flex-col mx-10 min-h-[calc(100vh-6rem)]">
             <div className="lg:w-1/2 welcome-text">
                 <h1 className="text-5xl font-bold mb-8">Welcome to Our Website</h1>
                 <p className="text-lg mb-8">
@@ -27,16 +27,16 @@ export default function Home() {
                     our community!
                 </p>
                 <button
-                    className="bg-gradient-to-r from-purple-600 to-blue-600 px-6 py-3 rounded-md font-semibold hover:scale-105 transition-transform"
+                    className="text-white bg-gradient-to-r from-purple-600 to-blue-600 px-6 py-3 rounded-md font-semibold hover:scale-105 transition-transform"
                     onClick={() => navigate('/challenges')}
                 >
                     Get Started
                 </button>
             </div>
-            <div className="lg:w-1/2 max-lg:mt-5 welcome-img">
+            <div className="lg:w-1/2 max-lg:mt-5 items-center flex justify-center">
                 <img
-                    className="rounded-lg shadow-md hover:shadow-xl transition-shadow"
-                    src="https://media.nature.com/lw800/magazine-assets/d41586-019-00653-5/d41586-019-00653-5_16459152.jpg"
+                    className="welcome-img rounded-lg h-[35rem] object-contain"
+                    src={require("../asset/coding.webp")}
                     alt="coding"
                 />
             </div>
